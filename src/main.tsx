5443cd71a3b8df8699c8614f06b00f6c72ab824a
qiuxing <qiuxing@nolibox.com>
2023-08-23 10:14:17 +0800

    feat: migrate code

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import App from "./App";
+import LoadApp from "./components/LoadApp";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <LoadApp>
+    <App />
+  </LoadApp>,
+);
